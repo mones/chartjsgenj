50ca809fac52111181214aab0e1ac1c083d5f870
Ricardo Mones <ricardo@mones.org>
2015-09-18 21:19:09 +0200

    Test writting complete pages

--- test/org/mones/chartjsgenj/ChartTest.java
+++ test/org/mones/chartjsgenj/ChartTest.java
@@ -89,4 +89,198 @@ public class ChartTest {
 		System.out.println(sc);
 		*/
 	}
+
+	@Test
+	public void testHtmlScriptFullPage() {
+		String fname = TEST_WRITE_DIR + File.separator + "test1.html";
+		Writer w = null;
+		try {
+			// prepare
+			OptionsLine ol = new OptionsLine();
+			OptionsBar ob = new OptionsBar();
+			OptionsRadar or = new OptionsRadar();
+			OptionsPolarArea opa = new OptionsPolarArea();
+			OptionsPieDoughnut opd = new OptionsPieDoughnut();
+			Chart c = new Chart(ChartType.LINE);
+			c.setLabels(Arrays.asList(new String[] {
+				"January", "February", "March", "April", "May", "June", "July"
+			}));
+			Dataset d1 = new Dataset("My First dataset");
+			d1.setFillColor(new Colour(220, 0.2f));
+			d1.setStrokeColor(new Colour(220));
+			d1.setPointColor(new Colour(220));
+			d1.setPointStrokeColor(new Colour("#fff"));
+			d1.setPointHighlightFill(new Colour("#fff"));
+			d1.setPointHighlightStroke(new Colour(220));
+			d1.setData(new int[] { 65, 59, 80, 81, 56, 55, 40 });
+			c.addDataset(d1);
+			Dataset d2 = new Dataset("My Second dataset");
+			d2.setFillColor(new Colour(151, 187, 205, 0.2f));
+			d2.setStrokeColor(new Colour(151, 187, 205));
+			d2.setPointColor(new Colour(151, 187, 205));
+			d2.setPointStrokeColor(new Colour("#fff"));
+			d2.setPointHighlightFill(new Colour("#fff"));
+			d2.setPointHighlightStroke(new Colour(151, 187, 205));
+			d2.setData(new int[] { 28, 48, 40, 19, 86, 27, 90 });
+			c.addDataset(d2);
+			Dataset[] pads = new Dataset[] {
+				new Dataset("Red", 300, "#F7464A", "#FF5A5E"),
+				new Dataset("Green", 50, "#46BFBD", "#5AD3D1"),
+				new Dataset("Yellow", 100, "#FDB45C", "#FFC870"),
+				new Dataset("Grey", 40, "#949FB1", "#A8B3C5"),
+				new Dataset("Dark Grey", 120, "#4D5360", "#616774"),
+			};
+			// constants
+			final String canvasid = "test";
+			final String canvasid2 = "test2";
+			final String canvasid3 = "test3";
+			final String canvasid4 = "test4";
+			final String canvasid5 = "test5";
+			// write
+			w = new BufferedWriter(new OutputStreamWriter(
+				new FileOutputStream(fname), "utf-8"));
+			w.write("<html>\n<head>\n<title>Test</title>\n");
+			w.write(Chart.htmlScript("Chart.js/", false));
+			w.write("</head>\n<body>\n");
+			w.write(c.htmlCanvas(canvasid , 710, 355));
+			w.write(c.htmlScript(canvasid, ol));
+			w.write("<br>");
+			c.setType(ChartType.BAR);
+			d1.getFillColor().setA(0.5f);
+			d1.setHighlightFill(new Colour(220,0.75f));
+			d1.setHighlightStroke(new Colour(220));
+			d2.getFillColor().setA(0.5f);
+			d2.setHighlightFill(new Colour(151, 187, 205, 0.75f));
+			d2.setHighlightStroke(new Colour(151, 187, 205));
+			w.write(c.htmlCanvas(canvasid2 , 710, 355));
+			w.write(c.htmlScript(canvasid2, ob));
+			w.write("<br>");
+			c.setType(ChartType.RADAR);
+			c.setLabels(Arrays.asList(new String[] {
+				"Eating", "Drinking", "Sleeping", "Designing", "Coding", "Cycling", "Running"
+			}));
+			d1.getFillColor().setA(0.2f);
+			d2.getFillColor().setA(0.2f);
+			w.write(c.htmlCanvas(canvasid3 , 710, 355));
+			w.write(c.htmlScript(canvasid3, or));
+			w.write("<br>");
+			c = new Chart(ChartType.POLAR_AREA);
+			c.setDatasets(Arrays.asList(pads));
+			w.write(c.htmlCanvas(canvasid4 , 710, 355));
+			w.write(c.htmlScript(canvasid4, opa));
+			w.write("<br>");
+			Chart pie = new Chart(ChartType.PIE);
+			pie.addDataset(pads[0]);
+			pie.addDataset(pads[1]);
+			pie.addDataset(pads[2]);
+			Chart dou = new Chart(ChartType.DOUGHNUT);
+			dou.addDataset(pads[0]);
+			dou.addDataset(pads[1]);
+			dou.addDataset(pads[2]);
+			w.write(pie.htmlCanvas(canvasid5 + "pie" , 352, 176));
+			w.write(dou.htmlCanvas(canvasid5 + "dou" , 352, 176));
+			w.write(pie.htmlScript(canvasid5 + "pie", opd));
+			w.write(dou.htmlScript(canvasid5 + "dou", opd));
+			w.write("</body>\n</html>");
+		} catch (UnsupportedEncodingException e) {
+			Assert.fail(e.getMessage());
+		} catch (FileNotFoundException e) {
+			Assert.fail(e.getMessage());
+		} catch (IOException e) {
+			Assert.fail(e.getMessage());
+		} finally {
+			try {
+				if (w != null) {
+					w.flush();
+					w.close();
+				}
+			} catch (IOException e) {
+				Assert.fail(e.getMessage());
+			}
+		}
+		// check file size
+		Assert.assertEquals(6098, (new File(fname)).length());
+	}
+
+	@Test
+	public void testHtmlScriptThemes() {
+		String fname = TEST_WRITE_DIR + File.separator + "test2.html";
+		Writer w = null;
+		try {
+			// prepare
+			OptionsLine ol = new OptionsLine();
+			Chart c = new Chart(ChartType.LINE);
+			c.setLabels(Arrays.asList(new String[] {
+				"January", "February", "March", "April", "May", "June", "July"
+			}));
+			Dataset[] data = new Dataset[] {
+				new Dataset("My First dataset",
+					new int[] { 65, 59, 80, 81, 56, 55, 40 }),
+				new Dataset("My Second dataset",
+					new int[] { 28, 48, 40, 19, 86, 27, 90 }),
+				new Dataset("My Third dataset",
+					new int[] { 18, 19, 45, 44, 43, 32, 33 }),
+			};
+			c.setDatasets(Arrays.asList(data));
+			Colour ca = new Colour(138, 179, 232);
+			Colour cb = new Colour(95, 159, 242);
+			Colour cc = new Colour(49, 137, 253);
+			Theme t = new Theme(ca.toHTML() + "," + cb.toHTML() + "," + cc.toHTML());
+			t.apply(c);
+			// constants
+			final String canvasid = "test";
+			final String canvasid2 = "test2";
+			// write
+			w = new BufferedWriter(new OutputStreamWriter(
+				new FileOutputStream(fname), "utf-8"));
+			w.write("<html>\n<head>\n<title>Test</title>\n");
+			w.write(Chart.htmlScript("Chart.js/", false));
+			w.write(Chart.htmlLegendStyle(
+				"list-style-type: none; background-color: #eee; padding: 10px; width: 100px;",
+				"display: inline;",
+				"display: block; width: 16px; min-height: 16px;"));
+			w.write("</head>\n<body>\n");
+			w.write(c.htmlCanvas(canvasid , 710, 355));
+			w.write(c.htmlScript(canvasid, ol));
+			w.write("<br>");
+			List<String> labels = Arrays.asList(new String[] {
+				"One", "Two", "Three", "Four", "Five", "Six", "Seven"
+			});
+			List<Number> values = new ArrayList<Number>();
+			for (int i = 0; i < labels.size(); ++i) {
+				values.add(new Float(Math.round(10 + labels.size() * 10 * Math.random())));
+			}
+			List<Dataset> data2 = Dataset.fromLabelsValues(labels, values);
+			Chart c2 = new Chart(ChartType.DOUGHNUT, true);
+			c2.setDatasets(data2);
+			Theme t2 = new Theme(Theme.SOLARIZED_ACCENT);
+			t2.apply(c2);
+			OptionsPieDoughnut ops = new OptionsPieDoughnut();
+			ops.animateScale = true;
+			ops.showScale = true;
+			w.write(HTML.tableLeftRight(
+					c2.htmlCanvas(canvasid2 , 500, 450),
+					c2.htmlLegend(canvasid2)
+					));
+			w.write(c2.htmlScript(canvasid2, ops));
+			w.write("</body>\n</html>");
+		} catch (UnsupportedEncodingException e) {
+			Assert.fail(e.getMessage());
+		} catch (FileNotFoundException e) {
+			Assert.fail(e.getMessage());
+		} catch (IOException e) {
+			Assert.fail(e.getMessage());
+		} finally {
+			try {
+				if (w != null) {
+					w.flush();
+					w.close();
+				}
+			} catch (IOException e) {
+				Assert.fail(e.getMessage());
+			}
+		}
+		// check file size
+		Assert.assertEquals(5881, (new File(fname)).length());
+	}
 }
