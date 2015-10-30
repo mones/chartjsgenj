d164bd191d5b30e8e5eecf4201f4801d310a594f
Ricardo Mones <ricardo@mones.org>
2015-10-31 00:02:47 +0100

    Make info more readable on javadoc

--- src/org/mones/chartjsgenj/package-info.java
+++ src/org/mones/chartjsgenj/package-info.java
@@ -1,7 +1,14 @@
-/*
- * ChartjsGenj - Cheap an dirty Chart.js generator for Java.
- * Copyright (c) 2013-2015, Ricardo Mones <ricardo@mones.org>
- * All rights reserved.
- * Read the accompanying LICENCE file for usage details.
+/**
+ * <h2>ChartjsGenj - Cheap an dirty Chart.js generator for Java.</h2>
+ * <p>
+ * Copyright (c) 2013-2015, Ricardo Mones &lt;ricardo@mones.org&gt;<br>
+ * All rights reserved.<br>
+ * Read the accompanying LICENCE file for usage details.<br>
+ * </p>
+ * <p>
+ * This is a quick implementation of a HTML/JavaScript generator for writting
+ * web pages with Chart.js charts. Not everything is supported, and the design
+ * can surely be improved, but so far it works for me <tt>;-)</tt>
+ * </p>
  */
 package org.mones.chartjsgenj;
