7db1f45900a56fe346a98ac583084435b02e2893
Ricardo Mones <ricardo@mones.org>
2015-10-30 23:08:00 +0100

    Add a licence

--- src/org/mones/chartjsgenj/OptionsPieDoughnut.java
+++ src/org/mones/chartjsgenj/OptionsPieDoughnut.java
@@ -1,3 +1,9 @@
+/*
+ * ChartjsGenj - Cheap an dirty Chart.js generator for Java.
+ * Copyright (c) 2013-2015, Ricardo Mones <ricardo@mones.org>
+ * All rights reserved.
+ * Read the accompanying LICENCE file for usage details.
+ */
 package org.mones.chartjsgenj;
 
 import java.lang.reflect.Field;
